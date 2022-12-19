463fa07b753bd96d82dde2f77c62c91a20453a3f
Colton Christopher <2tontide@gmail.com>
2022-12-18 20:55:20 -0600

    Completed

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numberOfWheels;
+            bool input = false;
+
+            do
+            {
+                Console.WriteLine("Enter the amount of tires for the vehicle you want to create:");
+
+                input = int.TryParse(Console.ReadLine(), out numberOfWheels);
+
+            } while (input == false);
+
+            var vehicle = VehicleFactory.GetVehicle(numberOfWheels);
+            vehicle.Drive();
         }
     }
 }
